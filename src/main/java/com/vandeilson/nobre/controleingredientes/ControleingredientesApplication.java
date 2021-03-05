1d1e1dd81c833b6486e965002828bfe070ffa3dd
Vandeilson L Nobre <vandernobrel@gmail.com>
2021-03-05 11:23:03 -0300

    initial commit

---
+++ src/main/java/com/vandeilson/nobre/controleingredientes/ControleingredientesApplication.java
@@ -0,0 +1,13 @@
+package com.vandeilson.nobre.controleingredientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleingredientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleingredientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vandeilson/nobre/controleingredientes/ControleingredientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandeilson.nobre.controleingredientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleingredientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
